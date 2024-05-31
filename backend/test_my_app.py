fb93c52d119148a044be9af62aba02fd4382f422
D-Zane <theangryzane@gmail.com>
2024-05-31 04:26:51 +0300

    Добавлен тест

--- backend/test_my_app.py
+++ backend/test_my_app.py
@@ -1,28 +1,45 @@
 import unittest
-import app 
+from io import BytesIO
+import wave
+from app import app
+import os
 
+class FlaskFileUploadTest(unittest.TestCase):
 
-
-class TestApp(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
+        self.app.testing = True
+
+    def tearDown(self):
+        # Удалить все файлы из папки загрузки после каждого теста
+        folder = app.config['UPLOAD_FOLDER']
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+
+    def create_wav_file(self):
+        buffer = BytesIO()
+        with wave.open(buffer, 'wb') as wf:
+            wf.setnchannels(1)  # mono
+            wf.setsampwidth(2)  # 16-bit
+            wf.setframerate(16000)  # 16 kHz
+            wf.writeframes(b'\x00\x00' * 16000)  # 1 second of silence
+        buffer.seek(0)
+        return buffer
 
     def test_upload_file(self):
-        # Сначала загружаем файл
-        with open('./uploads/2830-3980-0043.wav', 'rb') as f:
-            rv = self.app.post('/upload', data={'file': f})
-        # Проверяем код ответа
-        self.assertEqual(rv.status_code, 200)
-        # Проверяем, что на странице результатов есть текст
-        self.assertIn(b'Transcription Result', rv.data)
+        wav_file = self.create_wav_file()
+        data = {
+            'file': (wav_file, 'test.wav')
+        }
+        response = self.app.post('/upload', content_type='multipart/form-data', data=data)
 
-    def test_empty_upload(self):
-        # Тест на загрузку пустого файла
-        rv = self.app.post('/upload', data={'file': None})
-        # Проверяем код ответа
-        self.assertEqual(rv.status_code, 400)
-        # Проверяем, что на странице ошибки есть текст
-        self.assertIn(b'Error: No file provided', rv.data)
+        # Проверка, что запрос успешен и был перенаправлен на результат
+        self.assertEqual(response.status_code, 200)
+
+        # Проверка, что файл действительно был сохранен
+        self.assertTrue(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], 'test.wav')))
 
 if __name__ == '__main__':
     unittest.main()
